ddfdb2e4e9c7298969cda1d0b051185f24e23694
sverrir <sverrir1993@gmail.com>
2016-04-30 16:37:18 +0000

    The player can now pick up rocks from pile

--- AbominableSnowmanSource/Assets/Scripts/CharacterController.cs
+++ AbominableSnowmanSource/Assets/Scripts/CharacterController.cs
@@ -6,14 +6,29 @@ public class CharacterController : MonoBehaviour {
 
     private Animator anim;
     private Rigidbody2D rb;
+    [SerializeField] private GameObject objectSlot;
+    [SerializeField] private GameObject throwableObject;
+
+    public bool CanPickUpRocks {get; set;}
 
     [SerializeField] private float maxSpeed = 10f;
     [SerializeField] private bool facingRight = false;
+    private bool isHoldingObject = false;
 
     void Start () {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-	}
+
+        if (objectSlot == null)
+            Debug.Log("Player object slot not found");
+        if (throwableObject == null)
+            Debug.Log("Rock gameobject not found");
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+            PickUpRock();
+    }
 	
 	void FixedUpdate () {
         float move = Input.GetAxis("Horizontal");
@@ -31,5 +46,13 @@ public class CharacterController : MonoBehaviour {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void PickUpRock() {
+        if (CanPickUpRocks && !isHoldingObject) {
+            isHoldingObject = true;
+            GameObject rock = (GameObject)Instantiate(throwableObject, objectSlot.transform.position, objectSlot.transform.rotation);
+            rock.transform.parent = objectSlot.transform;
+        }
     }
 }
---
+++ AbominableSnowmanSource/Assets/Scripts/RockPile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class RockPile : MonoBehaviour {
+
+    private SpriteRenderer render;
+    private Color originalColor;
+
+    [SerializeField] private Color triggerColor;
+
+	void Start () {
+        foreach (Transform child in transform) {
+            if (child.name.Equals("graphics")) {
+                render = child.GetComponent<SpriteRenderer>();
+                break;
+            }
+        }
+        originalColor = render.material.color;
+    }
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            render.material.color = triggerColor;
+            CharacterController playerController = GetPlayerController(other);
+            playerController.CanPickUpRocks = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            CharacterController playerController = GetPlayerController(other);
+            render.material.color = originalColor;
+            playerController.CanPickUpRocks = false;
+        }
+    }
+
+    private CharacterController GetPlayerController(Collider2D other) {
+        CharacterController controller = other.gameObject.GetComponent<CharacterController>();
+
+        if (controller == null)
+            Debug.Log("No CharacterController found!");
+
+        return controller;
+    }
+}
