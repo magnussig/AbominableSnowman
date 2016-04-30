f09d927575868f8d262d59ca191aaa18e34a4f7a
sverrir <sverrir1993@gmail.com>
2016-04-30 17:34:41 +0000

    instruction text now appears over rock pile when player is in its proximity

--- AbominableSnowmanSource/Assets/Scripts/RockPile.cs
+++ AbominableSnowmanSource/Assets/Scripts/RockPile.cs
@@ -5,22 +5,29 @@ public class RockPile : MonoBehaviour {
 
     private SpriteRenderer render;
     private Color originalColor;
+    private TextMesh instructions;
+    private string instructionsText;
 
     [SerializeField] private Color triggerColor;
+    [SerializeField] private KeyCode keycode;
+
 
 	void Start () {
         foreach (Transform child in transform) {
-            if (child.name.Equals("graphics")) {
+            if (child.name.Equals("graphics"))
                 render = child.GetComponent<SpriteRenderer>();
-                break;
-            }
+            else if (child.name.Equals("instructions"))
+                instructions = child.GetComponent<TextMesh>(); 
         }
         originalColor = render.material.color;
+        instructionsText = "[ Press " + keycode.ToString() + " to pick up rock ]";
     }
-	
-	void Update () {
-	
-	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            instructions.text = instructionsText;
+        }
+    }
 
     void OnTriggerStay2D(Collider2D other) {
         if (other.tag == "Player") {
@@ -31,11 +38,11 @@ public class RockPile : MonoBehaviour {
     }
 
     void OnTriggerExit2D(Collider2D other) {
-        if (other.tag == "Player")
-        {
+        if (other.tag == "Player") {
             CharacterController playerController = GetPlayerController(other);
             render.material.color = originalColor;
             playerController.CanPickUpRocks = false;
+            instructions.text = "";
         }
     }
 
