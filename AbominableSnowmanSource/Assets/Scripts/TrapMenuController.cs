334a474de1e71e5042d3b9979a36c469a087c513
magnussig <maggisig92@gmail.com>
2016-05-10 11:24:44 +0000

    songs and cloud delete

--- AbominableSnowmanSource/Assets/Scripts/TrapMenuController.cs
+++ AbominableSnowmanSource/Assets/Scripts/TrapMenuController.cs
@@ -4,37 +4,11 @@ using System.Collections;
 public class TrapMenuController : MonoBehaviour {
     
     public GameObject blizzard;
-    private GameObject blizzardInstance;
+    private TrapPlacementScript tp;
 
     public void Clicked()
     {
-        blizzardInstance = Instantiate(blizzard);
-        blizzard.SetActive(true);
-
-        /*String
-        if (objectClicked.Equals("Blizzard"))
-        {
-            GameObject.Find("Blizzard").SetActive(true);
-        }
-        else
-        {
-            Debug.Log(objectClicked);
-        }*/
-    }
-
-    private void OnDisable()
-    {
-        //TODO: fix error
-        TrapPlacementScript tp = blizzardInstance.GetComponent<TrapPlacementScript>();
-        if (tp.isActiveAndEnabled)
-        {
-            Destroy(tp);
-        }
-    }
-
-    private void OnEnable()
-    {
-        //tékka á null
-        blizzardInstance = null;
+        tp = Instantiate(blizzard).GetComponent<TrapPlacementScript>();
+        
     }
 }
