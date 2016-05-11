b421fbd773c82ba7aaa0c6c711c10a0aeb1aedf5
sverrir <sverrir1993@gmail.com>
2016-05-11 16:08:20 +0000

    pull

---
+++ AbominableSnowmanSource/Assets/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UIManager : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+	void Update () {
+        anim.SetBool("In", true);
+	}
+}
