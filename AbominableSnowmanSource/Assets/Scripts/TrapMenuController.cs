d022bb2abfaba145fcd37db901399e8363aaae60
magnussig <maggisig92@gmail.com>
2016-05-11 15:21:49 +0000

    cost deducted when items bought and hover stuff

--- AbominableSnowmanSource/Assets/Scripts/TrapMenuController.cs
+++ AbominableSnowmanSource/Assets/Scripts/TrapMenuController.cs
@@ -6,8 +6,14 @@ public class TrapMenuController : MonoBehaviour
 {
 
     public GameObject blizzard;
-    [SerializeField]
-    private int healthPoints;
+    [SerializeField]    private int healthPoints;
+    private GameManager gm;
+    private GameObject player;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
 
     public void Clicked(string objectClicked)
     {
@@ -16,10 +22,13 @@ public class TrapMenuController : MonoBehaviour
         if (objectClicked == "Blizzard")
         {
             Instantiate(blizzard);
+            //deduct from score inside TrapPlacementScript to get mouse position
         }
         else if (objectClicked == "Health")
         {
             player.addHealthPoints(healthPoints);
+
+            gm.deductFromScore(100, transform);
         }
     }
 }
