b291938562950713fc89ae0891b0c0f2e9a39b12
huldar <huldarbjarmi@gmail.com>
2016-05-16 17:01:02 +0000

    highscore

--- AbominableSnowmanSource/Assets/Scripts/UIManager.cs
+++ AbominableSnowmanSource/Assets/Scripts/UIManager.cs
@@ -13,12 +13,23 @@ public class UIManager : MonoBehaviour {
     [SerializeField] private Text GameOverWave;
     [SerializeField] private Text GameOverKillCount;
     [SerializeField] private Text GameOverScore;
+    [SerializeField] private Text GameOverHeader;
+    [SerializeField] private Text GameOverSubmit;
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private Image background;
+    [SerializeField] private InputField input;
 
-    public int highscore;
-    public int highwave;
+    public int highscore1;
+    public int highwave1;
+    public int highscore2;
+    public int highwave2;
+    public int highscore3;
+    public int highwave3;
+    public string nickname1;
+    public string nickname2;
+    public string nickname3;
+    int position;
     private bool isGameOverShowing;
     private bool isPauseMenuShowing;
     private InstructionsController instructionController;
@@ -27,8 +38,16 @@ public class UIManager : MonoBehaviour {
     private GameManager gManager;
 
     void Start() {
-        highscore = PlayerPrefs.GetInt("highscore", 0);
-        highwave = PlayerPrefs.GetInt("highwave", 0);
+        input.characterLimit = 8;
+        nickname1 = PlayerPrefs.GetString("nickname1", "");
+        nickname2 = PlayerPrefs.GetString("nickname2", "");
+        nickname3 = PlayerPrefs.GetString("nickname3", "");
+        highscore1 = PlayerPrefs.GetInt("highscore1", 0);
+        highwave1 = PlayerPrefs.GetInt("highwave1", 0);
+        highscore2 = PlayerPrefs.GetInt("highscore2", 0);
+        highwave2 = PlayerPrefs.GetInt("highwave2", 0);
+        highscore3 = PlayerPrefs.GetInt("highscore3", 0);
+        highwave3 = PlayerPrefs.GetInt("highwave3", 0);
         gManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
         anim = GetComponent<Animator>();
         instructionController = GameObject.FindGameObjectWithTag("Instruction").GetComponent<InstructionsController>();
@@ -64,16 +83,16 @@ public class UIManager : MonoBehaviour {
     }
 
     public void UpdateCounters(int waveCount, int hazardpoints) {
-        waveCounter.text = "Wave: " + waveCount;
-        hazardPoints.text = "Hazard Points: " + hazardpoints;
+        waveCounter.text = "Wave : " + waveCount;
+        hazardPoints.text = "Hazard Points : " + hazardpoints;
     }
 
     public void UpdateCountDown(int count) {
-        waveCountDown.text = "Next wave starts in: " + count;
+        waveCountDown.text = "Next wave starts in : " + count;
     }
 
     public void UpdateSpawnStats(int numberspawned, int total) {
-        spawnStats.text = "Enemies: " + numberspawned + "/" + total;
+        spawnStats.text = "Enemies : " + numberspawned + "/" + total;
     }
 
     public void SetGameOverStats(int wave, int killed, int score) {
@@ -106,12 +125,57 @@ public class UIManager : MonoBehaviour {
 
     public void IsHighScore(int waveCount,int score)
     {
-        if(waveCount > highwave &&  score > highscore)
+        
+        if(waveCount >= highwave1 &&  score > highscore1)
         {
-            PlayerPrefs.SetInt("highwave", waveCount);
-            PlayerPrefs.SetInt("highscore", score);
+            position = 1;
+            Congrats();
+            PlayerPrefs.SetInt("highwave3", highwave2);
+            PlayerPrefs.SetInt("highscore3", highscore2);
+            PlayerPrefs.SetInt("highwave2", highwave1);
+            PlayerPrefs.SetInt("highscore2", highscore1);
+            PlayerPrefs.SetInt("highwave1", waveCount);
+            PlayerPrefs.SetInt("highscore1", score);
+        }
+        else if(waveCount >= highwave2 && score > highscore2)
+        {
+            position = 2;
+            Congrats();
+            PlayerPrefs.SetInt("highwave3", highwave2);
+            PlayerPrefs.SetInt("highscore3", highscore2);
+            PlayerPrefs.SetInt("highwave2", waveCount);
+            PlayerPrefs.SetInt("highscore2", score);
+        }
+        else if(waveCount >= highwave3 && score > highscore3)
+        {
+            position = 3;
+            Congrats();
+            PlayerPrefs.SetInt("highwave3", waveCount);
+            PlayerPrefs.SetInt("highscore3", score);
         }
 
+    }
+
+    public void Congrats()
+    {
+        if(position == 1)
+        {
+            nickname1 = input.text;
+            PlayerPrefs.SetString("nickname1", nickname1);
+        }
+        else if (position == 2)
+        {
+            nickname2 = input.text;
+            PlayerPrefs.SetString("nickname2", nickname2);
+        }
+
+        else if (position == 3)
+        {
+            nickname3 = input.text;
+            PlayerPrefs.SetString("nickname3", nickname3);
+        }
+        GameOverHeader.text = "Congratulations you made it to the top 3!";
+        GameOverSubmit.text = "Submit";
     }
 
 
